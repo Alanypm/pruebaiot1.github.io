f71e4ab47d4a8fd0d35569f1fd4d4e830a2d681d
Alanypm <81442841+Alanypm@users.noreply.github.com>
2021-03-31 10:50:00 -0600

    Delete init.js

--- js/init.js
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-firebase.initializeApp({
-  apiKey: "AIzaSyBOSm4TpsDPwWRjXvHA2DRoBNar2k5Z0aA",
-  authDomain: "pruebaiot1.firebaseapp.com",
-  projectId: "pruebaiot1",
-  storageBucket: "pruebaiot1.appspot.com",
-  messagingSenderId: "452462301531",
-  appId: "1:452462301531:web:8c7b23f750b77d977dca7e"
-});
